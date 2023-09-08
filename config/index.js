74a561c36f12fd168c1b5e427f5ff33609660a9b
Domey Benjamin <domeybenjamin1@gmail.com>
2023-09-08 00:13:51 +0000

    feat: updated confif

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 exports.CONFIG = {
-  NODE_ENV: 'development',
+  NODE_ENV: process.env.NODE_ENV ?? 'development',
   APP_NAME: 'gnpc-attendance',
-  PORT: 5001,
-  MONGO_URI: 'mongodb://localhost:27017/gnpc-dev',
+  PORT: process.env.PORT ?? 5001,
+  MONGO_URI: process.env.MONGO_URI ?? 'mongodb://localhost:27017/gnpc-dev',
   VERFICATION_CODE: {
     ALPHABET: '0123456789',
     LENGTH: 5,
